9a9732b9f58b74452b67574415d56c6a96506c8b
towneh <matt.town@gmail.com>
2021-02-12 23:41:43 +0000

    Update TelePlayerInteract.cs

--- TelePlayerInteract.cs
+++ TelePlayerInteract.cs
@@ -7,11 +7,11 @@ using VRC.Udon;
 public class TelePlayerInteract : UdonSharpBehaviour
 {
 	public Transform[] teleportTargets;
-		
-    public override void Interact()
-    {
+	
+	public override void Interact()
+	{
 		int i;
 		i = Random.Range(0, teleportTargets.Length);
 		Networking.LocalPlayer.TeleportTo(teleportTargets[i].position, teleportTargets[i].rotation);
-    }
+	}
 }
