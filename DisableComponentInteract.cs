5b7e25caf5750cf05ea07ef8bfc9720812e87af1
towneh <matt.town@gmail.com>
2021-03-11 16:12:51 +0000

    Add files via upload

---
+++ DisableComponentInteract.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DisableComponentsInteract : UdonSharpBehaviour
+{
+	//public Button[] disableComponents;
+	public GameObject[] disableComponents;
+	public GameObject[] enableComponents;
+
+	public void Interact()
+    {
+	/*	foreach(Button button in disableComponents) {
+			if(button.gameObject.activeInHierarchy); button.onClick.Invoke();
+		}*/ //we hate making our life easy don't we udon
+		foreach (GameObject go in disableComponents) {
+			if(go.activeInHierarchy); go.SetActive(false);
+		}
+		foreach (GameObject go in enableComponents) {
+			if(go.activeInHierarchy); go.SetActive(true);
+		}
+	}
+}
