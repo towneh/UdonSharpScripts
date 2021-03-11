dff668fe230e256f27737b484a2fe7f77a5b015f
towneh <matt.town@gmail.com>
2021-03-11 16:14:20 +0000

    Add files via upload

---
+++ ModPlayers.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ModPlayers : UdonSharpBehaviour
+{
+	private InputField playerName;
+	public Text textTarget;
+	
+    void Interact()
+    {
+		textTarget.text = this.playerName.text;
+    }
+}
