210ddd955b74ef68f71073309996331118eb738f
towneh <matt.town@gmail.com>
2021-03-11 16:13:32 +0000

    Add files via upload

---
+++ MenuButtonEnable.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MenuButtonEnable : UdonSharpBehaviour
+{
+	public GameObject toggleButtonOff;
+	public GameObject toggleButtonOn;
+	public AudioSource audioEnableSrc;
+	public AudioClip audioEnableClip;
+	
+    void Interact()
+    {
+		if (toggleButtonOff.activeInHierarchy)
+		{
+			toggleButtonOff.SetActive(false);
+			toggleButtonOn.SetActive(true);
+			audioEnableSrc.clip = audioEnableClip;
+			audioEnableSrc.Play();
+		} 
+    }
+}
