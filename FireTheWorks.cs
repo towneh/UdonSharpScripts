0a2029261ad917762b4936d06bc0761fb61c9a23
towneh <matt.town@gmail.com>
2021-06-27 12:39:58 +0100

    Update FireTheWorks.cs

--- FireTheWorks.cs
+++ FireTheWorks.cs
@@ -14,55 +14,60 @@ public class FireTheWorks : UdonSharpBehaviour
     private bool _cooldownActive;
     
     public override void Interact()
-	  {
-		  CheckCooldown();
-		  if (_cooldownActive) return;
-		  else {
-		    SendCustomNetworkEvent(NetworkEventTarget.All, "TriggerTarget");
-		  }
-	  }
+    {
+        CheckCooldown();
+	if (_cooldownActive) return;
+	else {
+	    SendCustomNetworkEvent(NetworkEventTarget.All, "TriggerTarget");
+        }
+    }
 	
-	  public override void OnPlayerJoined(VRCPlayerApi player)
-	  {
-		  if (Networking.IsMaster)
-		  {
-			  if (_cooldownActive)
-			  {
-				  //How do I pass "cooldownStart" float value to late joiner if "cooldownActive" boolean is true?
-			  }
-		  }
-	  }
-	
-	  public void UpdateCooldown()
-	  {
-		  _cooldownStart = Networking.GetServerTimeInMilliseconds();
-	  }
-	
-	  public void CheckCooldown()
-	  {
-		  _currentTime = Networking.GetServerTimeInMilliseconds();
-		  if (_cooldownStart == 0) {
-			  _cooldownActive = false;
-		  }
-		  // spam delay of 15 minutes between each firework session
-		  else if (_currentTime - _cooldownStart <= 900000) { 
-			  _cooldownActive = true;
-		  }
-		  else {
-			  _cooldownActive = false;
-		  }
-	  }
-	
-	public void TriggerTarget()
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if (Networking.IsMaster)
 	{
-		UpdateCooldown();
-		target.SetActive(true);
-		soundEffect.Play();
-		SendCustomEventDelayedSeconds(nameof(DisableTarget), 17.0f);
-	}
+	    if (_cooldownActive)
+	    {
+		//How do I pass "cooldownStart" float value to late joiner if "cooldownActive" boolean is true?
+	        SendCustomNetworkEvent(NetworkEventTarget.All, "SerializeData");
+            }
+        }
+    }
 	
-	public void DisableTarget()
-	{
-		target.SetActive(false);
+    public void UpdateCooldown()
+    {
+        _cooldownStart = Networking.GetServerTimeInMilliseconds();
+    }
+	
+    public void CheckCooldown()
+    {
+	_currentTime = Networking.GetServerTimeInMilliseconds();
+	if (_cooldownStart == 0) {
+	    _cooldownActive = false;
 	}
+	// spam delay of 15 minutes between each firework session
+        else if (_currentTime - _cooldownStart <= 900000) { 
+	    _cooldownActive = true;
+	}
+	else {
+	    _cooldownActive = false;
+        }
+    }
+	
+    public void TriggerTarget()
+    {
+	UpdateCooldown();
+	target.SetActive(true);
+	soundEffect.Play();
+	SendCustomEventDelayedSeconds(nameof(DisableTarget), 17.0f);
+    }
+	
+    public void DisableTarget()
+    {
+	target.SetActive(false);
+    }
+    public void SerializeData()
+    {
+        RequestSerialization();
+    }
 }
