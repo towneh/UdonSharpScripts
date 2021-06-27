06654b593aedaaa2263ac628d0472c4439f57971
towneh <matt.town@gmail.com>
2021-06-27 11:55:20 +0100

    Update StageVoice.cs

--- StageVoice.cs
+++ StageVoice.cs
@@ -6,17 +6,32 @@ using VRC.Udon;
 
 public class StageVoice : UdonSharpBehaviour
 {
-    void override OnTriggerEnter()
+    [Header("Voice Area")]
+    [Tooltip("The collider which influences voice range")]
+    public Collider voiceBox;
+	
+    [Header("Player voice")]
+    [Tooltip("Adjusts the player volume")]
+    [Range(0f, 24f)]
+    public float voiceGain = 15f;
+
+    [Tooltip("The end of the range for hearing a user's voice")]
+    public float voiceFar = 25f;
+
+    [Tooltip("The near radius in meters where player audio starts to fall off, it is recommended to keep this at 0")]
+    public float voiceNear = 0f;
+	
+    public void OnTriggerEnter(Collider voiceBox)
     {
         
     }
 	
-    void override OnTriggerStay()
+    public void OnTriggerStay(Collider voiceBox)
     {
         
     }
 	
-    void override OnTriggerExit()
+    public void OnTriggerExit(Collider voiceBox)
     {
         
     }
