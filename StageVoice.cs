b0230ffca70f7ed21152f009a2f81f501ab18a1d
towneh <matt.town@gmail.com>
2021-06-27 11:34:33 +0100

    Create StageVoice.cs

---
+++ StageVoice.cs
@@ -0,0 +1,23 @@
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class StageVoice : UdonSharpBehaviour
+{
+    void override OnTriggerEnter()
+    {
+        
+    }
+	
+    void override OnTriggerStay()
+    {
+        
+    }
+	
+    void override OnTriggerExit()
+    {
+        
+    }
+}
