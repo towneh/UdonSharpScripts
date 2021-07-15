7956242d95b92b033999c26d5b0efdd9b4a859ac
towneh <matt.town@gmail.com>
2021-07-15 21:36:01 +0100

    Update TelePlayerCollider.cs

--- TelePlayerCollider.cs
+++ TelePlayerCollider.cs
@@ -6,21 +6,21 @@ using VRC.Udon;
 
 public class TelePlayerCollider : UdonSharpBehaviour
 {
-	public Transform[] teleportTargets;
+	public Transform[] _teleportTargets;
 		
 	public override void OnPlayerTriggerEnter(VRCPlayerApi player)
 	{
 		if(player != Networking.LocalPlayer) return;
 		int i;
-		i = Random.Range(0, teleportTargets.Length);
-		Networking.LocalPlayer.TeleportTo(teleportTargets[i].position, teleportTargets[i].rotation);
+		i = Random.Range(0, _teleportTargets.Length);
+		Networking.LocalPlayer.TeleportTo(_teleportTargets[i].position, _teleportTargets[i].rotation);
 	}
 
 	public override void OnPlayerTriggerStay(VRCPlayerApi player)
 	{
         	if(player != Networking.LocalPlayer) return;
 		int i;
-		i = Random.Range(0, teleportTargets.Length);
-		Networking.LocalPlayer.TeleportTo(teleportTargets[i].position, teleportTargets[i].rotation);
+		i = Random.Range(0, _teleportTargets.Length);
+		Networking.LocalPlayer.TeleportTo(_teleportTargets[i].position, _teleportTargets[i].rotation);
 	}
 }
