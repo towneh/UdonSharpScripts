13d398cf95fbe31ff5a8577e9839a0919c10d4e2
towneh <matt.town@gmail.com>
2021-07-30 00:10:35 +0100

    Update AccessStaff.cs

--- AccessStaff.cs
+++ AccessStaff.cs
@@ -6,26 +6,27 @@ using VRC.Udon;
 
 public class AccessStaff : UdonSharpBehaviour
 {
-	 public GameObject[] _basicAccess;
-   public GameObject[] _staffAccess;
-   public string[] _eventAdmins;
+    public GameObject[] _basicAccess;
+    public GameObject[] _staffAccess;
+    public string[] _eventAdmins;
 
-   public void Start()
-   {
-	    if (Networking.LocalPlayer.isInstanceOwner) {
-	       foreach(GameObject go in _basicAccess) {
-            go.SetActive(false);
-			   }
-		  }
-      foreach(string adminPlayers in _eventAdmins) {
-         if (Networking.LocalPlayer.displayName == _adminPlayers) {
-            foreach(GameObject go in _staffAccess) {
-               go.SetActive(true);
+    public void Start()
+    {
+        if (Networking.LocalPlayer.isInstanceOwner) {
+	    foreach(GameObject go in _basicAccess) {
+                go.SetActive(true);
+	    }
+	}
+        foreach(string _adminPlayers in _eventAdmins) {
+            if (Networking.LocalPlayer.displayName == _adminPlayers) {
+                //foreach(GameObject go in _staffAccess) {
+                //go.GetComponent<Collider>().isTrigger = true; 
+		//go.SetActive(true);
+                //}
+		foreach(GameObject go in _basicAccess) {
+		go.SetActive(true);
+		}
             }
-				    foreach(GameObject go in _basicAccess) {
-				       go.SetActive(false);
-				    }
-         }
-      }
-   }
-} 
+        }
+    }
+}
